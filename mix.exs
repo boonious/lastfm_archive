4d4ed121f74bbd67bd201ffb876a39a508b9d18e
Boon Low <boon.low@mac.com>
2018-10-11 14:20:13 +0100

    preps for coveralls reporting, travis CI

--- mix.exs
+++ mix.exs
@@ -1,13 +1,19 @@
 defmodule LastfmArchive.MixProject do
   use Mix.Project
 
+  @description """
+    A tool for creating local Last.fm scrobble data archive and analytics
+  """
+
   def project do
     [
       app: :lastfm_archive,
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
     ]
   end
 
@@ -21,8 +27,7 @@ defmodule LastfmArchive.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:excoveralls, "~> 0.10", only: :test}
     ]
   end
 end
