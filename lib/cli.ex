129c6fe71fcbc106b90c8647fdf82c1be9ed84b8
Boon Low <boon.low@mac.com>
2018-10-11 15:20:31 +0100

    preps for a command line tool; hex publication and ex_docs for documentation

---
+++ lib/cli.ex
@@ -0,0 +1,5 @@
+defmodule LastfmArchive.Cli do
+  @moduledoc """
+  """
+
+end
--- mix.exs
+++ mix.exs
@@ -11,23 +11,48 @@ defmodule LastfmArchive.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
+      escript: [main_module: LastfmArchive.Cli, path: "bin/lastfm_archive"],
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
+
+      # Docs
+      name: "lastfm_archive",
+      description: @description,
+      package: package(),
+      source_url: "https://github.com/boonious/lastfm_archive",
+      homepage_url: "https://github.com/boonious/lastfm_archive",
+      docs: [
+        main: "LastfmArchive",
+        extras: ["README.md", "CHANGELOG.md"]
+      ]
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:excoveralls, "~> 0.10", only: :test}
+      {:elixirfm, "~> 0.1.3"},
+      {:excoveralls, "~> 0.10", only: :test},
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false}
     ]
   end
+
+  defp package do
+    [
+      name: "lastfm_archive",
+      maintainers: ["Boon Low"],
+      licenses: ["Apache 2.0"],
+      links: %{
+        Changelog: "https://github.com/boonious/lastfm_archive/blob/master/CHANGELOG.md",
+        GitHub: "https://github.com/boonious/lastfm_archive"
+      }
+    ]
+  end
+
 end
