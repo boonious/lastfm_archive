599728b559c494c29c5100f7cabf5d72f364eeb5
Boon Low <boon.low@mac.com>
2018-11-02 16:44:52 +0000

    transform/3 raw JSON archive file into a list of tab-delimited track data

--- lib/transform.ex
+++ lib/transform.ex
@@ -1,13 +1,56 @@
 defmodule LastfmArchive.Transform do
   @moduledoc """
-  This module provides functions for reading and transforming the Lastfm data in archive
+  This module provides functions for reading and transforming downloaded Lastfm data.
 
   """
 
   @default_data_dir "./lastfm_data/"
+  @default_delimiter "\t"
 
+  @doc """
+  Transform a downloaded raw JSON page into a list of tab-delimited track data.
+
+  ### Example
+  ```
+    # transform a page of scrobbles from the file archive
+    LastfmArchive.transform("a_lastfm_user", "2007/200_1.gz")
+  ```
+  """
+  @spec transform(binary, binary, :atom) :: list(binary) | {:error, :file.posix}
+  def transform(user, filename, mode \\ :tsv)
+  def transform(user, filename, :tsv) do
+    {status, tracks} = read(user, filename)
+
+    case status do
+      :ok ->
+        index = initial_index(tracks["recenttracks"]["@attr"])
+        _transform(user, tracks["recenttracks"]["track"], index, [])
+      :error ->
+        {:error, tracks}
+    end
+  end
+
+  defp _transform(_user, [], _index, acc), do: acc
+  defp _transform(user, [track|tracks], index, acc) do
+    next_index = index + 1
+    _transform(user, tracks, next_index, acc ++ [_transform(user, track, index)])
+  end
+
+  # id,name,scrobble_date,mbid,url,artist,artist_mbid,artist_url,album,album_mid
+  defp _transform(user, track, index) do
+    id = "#{user}_#{track["date"]["uts"]}_#{index |> to_string}"
+
+    track_info = [ id, track["name"] |> String.trim, track["date"]["uts"], track["mbid"], track["url"] ]
+    artist_info = [ track["artist"]["name"],  track["artist"]["mbid"], track["artist"]["url"]]
+    album_info = [ track["album"]["#text"],  track["album"]["mbid"]]
+    Enum.join(track_info ++ artist_info ++ album_info, @default_delimiter)
+  end
+
+  @doc """
+  Read and parse a raw Lastfm JSON file from the archive for a Lastfm user.
+  """
   @spec read(binary, binary) :: {:ok, map} | {:error, :file.posix}
-  def read(user, filename \\ "1") do
+  def read(user, filename) do
     data_dir = Application.get_env(:lastfm_archive, :data_dir) || @default_data_dir
     user_data_dir = Path.join data_dir, user
     file_path = Path.join user_data_dir, filename
@@ -24,5 +67,7 @@ defmodule LastfmArchive.Transform do
     if is_pid(file_io), do: File.close(file_io)
     resp
   end
+
+  defp initial_index(info), do: ((String.to_integer(info["page"]) - 1) * String.to_integer(info["perPage"])) + 1
 
 end
--- test/transform_test.exs
+++ test/transform_test.exs
@@ -10,11 +10,18 @@ defmodule TransformTest do
     end
   end
 
-  test "read compressed archive data from a given file location" do
+  test "read compressed archive data for a given user, file location" do
     Application.put_env :lastfm_archive, :data_dir, @test_data_dir
 
     assert {:ok, lastfm_tracks} = LastfmArchive.Transform.read("test_user", "200_34.gz")
     assert length(lastfm_tracks["recenttracks"]["track"]) > 0
+  end
+
+  test "transform a page of compressed archive data for a given user, file location" do
+    Application.put_env :lastfm_archive, :data_dir, @test_data_dir
+
+    tracks = LastfmArchive.Transform.transform("test_user", "200_34.gz")
+    assert length(tracks) > 0
   end
 
 end
