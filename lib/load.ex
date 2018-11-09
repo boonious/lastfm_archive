246bd5aabdedce0441434e21efd6a81ebacdf1ac
Boon Low <boon.low@mac.com>
2018-11-09 10:43:59 +0000

    check_solr_schema/1 to ensure all required Lastfm data fields exist in Solr core or collection

--- lib/load.ex
+++ lib/load.ex
@@ -9,16 +9,16 @@ defmodule LastfmArchive.Load do
 
   The endpoint can either be a URL string or an atom referring to an endpoint in configuration.
   The library uses `Hui` to interact with Solr, an endpoint can be specified as below:
-  
+
   ### Example
-  
+
   ```
     LastfmArchive.Load.ping_solr("http://solr_url...")
-    LastfmArchive.Load.ping_solr(:lastfm_archive) # ping a configured endpoint 
+    LastfmArchive.Load.ping_solr(:lastfm_archive) # ping a configured endpoint
   ```
 
   `:lastfm_archive` refers to the following Solr update endpoint in configuration:
-  
+
   ```
     config :hui, :lastfm_archive,
       url: "http://solr_url..",
@@ -36,9 +36,57 @@ defmodule LastfmArchive.Load do
     case response do
       {:ok, resp} -> 
         if resp.status_code == 200, do: {:ok, resp.body |> Poison.decode!}, else: {:error, %Hui.Error{reason: :einval}}
-      {:error, %HTTPoison.Error{id: _, reason: reason}} -> 
+      {:error, %HTTPoison.Error{id: _, reason: reason}} ->
         {:error, %Hui.Error{reason: reason}}
     end
   end
-  
+
+  @doc """
+  Check a Solr core/collection to ensure it has the required Lastfm data fields.
+
+  The check currently inspects Solr schema for a list of Lastfm fields
+  and returns error if one or more of the fields are missing.
+  See `LastfmArchive.Transform.transform/3` for the list of fields.
+
+  ### Example
+
+  ```
+    LastfmArchive.Load.check_solr_schema("http://solr_url...")
+    LastfmArchive.Load.check_solr_schema(:lastfm_archive) # ping a configured endpoint
+  ```
+
+  See `ping_solr/1` for more details on URL configuration.
+  """
+  @spec check_solr_schema(binary|atom) :: {:ok, map} | {:error, Hui.Error.t}
+  def check_solr_schema(url) when is_atom(url), do: Application.get_env(:hui, url)[:url] |> check_solr_schema
+  def check_solr_schema(url) when is_binary(url) do
+    {:ok, schema_resp} = solr_schema(url)
+    schema = schema_resp["schema"]
+    fields = schema["fields"] |> Enum.map(&(&1["name"]))
+
+    {:ok, fields_s} = File.read("./solr/fields.json")
+    expected_fields = fields_s |> Poison.decode!
+
+    # simple check if field exists, no type checking for the time being
+    missing_fields = for {field, _type} <- expected_fields, do: unless Enum.member?(fields, field), do: field
+    missing_fields = missing_fields |> Enum.uniq |> List.delete(nil)
+
+    if length(missing_fields) > 0 do 
+      {:error, %Hui.Error{reason: :einit}}
+    else 
+      {:ok, expected_fields}
+    end
+  end
+
+  defp solr_schema(url) do
+    response = HTTPoison.get url <> "/schema"
+    
+    case response do
+      {:ok, resp} -> 
+        if resp.status_code == 200, do: {:ok, resp.body |> Poison.decode!}, else: {:error, %Hui.Error{reason: :ehostunreach}}
+      {:error, %HTTPoison.Error{id: _, reason: reason}} ->
+        {:error, %Hui.Error{reason: reason}}
+    end
+  end
+
 end
--- test/load_test.exs
+++ test/load_test.exs
@@ -1,6 +1,8 @@
 defmodule LoadTest do
   use ExUnit.Case, async: true
 
+  @expected_solr_fields_path Path.join ["solr", "fields.json"]
+
   setup do
     # true if mix test --include integration 
     is_integration = :integration in ExUnit.configuration[:include]
@@ -20,16 +22,61 @@ defmodule LoadTest do
     LastfmArchive.Load.ping_solr(url)
   end
 
-  test "ping Solr endpoint by config atom key", %{bypass: bypass} do
+  test "ping Solr endpoint by URL config key", %{bypass: bypass} do
     url = "http://localhost:#{bypass.port}/"
-    Application.put_env :hui, :lastfm_ping_test, url: url
+    Application.put_env :hui, :lastfm_ping_test1, url: url
 
     Bypass.expect bypass, fn conn ->
       assert conn.path_info == ["admin", "ping"]
       Plug.Conn.resp(conn, 200, "{}")
     end
 
-    LastfmArchive.Load.ping_solr(:lastfm_ping_test)
+    LastfmArchive.Load.ping_solr(:lastfm_ping_test1)
+  end
+
+  test "check Solr schema by URL string", %{bypass: bypass} do
+    url = "http://localhost:#{bypass.port}/"
+    expected_fields = File.read!(@expected_solr_fields_path) |> Poison.decode!
+
+    Bypass.expect bypass, fn conn ->      
+      schema_response = File.read!(Path.join ["test","data", "schema_response.json"])
+      assert conn.path_info == ["schema"]
+      Plug.Conn.resp(conn, 200, schema_response)
+    end
+
+    {status, fields} = LastfmArchive.Load.check_solr_schema(url)
+
+    assert status == :ok
+    assert expected_fields == fields
+  end
+
+  test "check Solr schema by URL config key", %{bypass: bypass} do
+    url = "http://localhost:#{bypass.port}/"
+    Application.put_env :hui, :lastfm_ping_test2, url: url
+    expected_fields = File.read!(@expected_solr_fields_path) |> Poison.decode!
+
+    Bypass.expect bypass, fn conn ->      
+      schema_response = File.read!(Path.join ["test","data", "schema_response.json"])
+      assert conn.path_info == ["schema"]
+      Plug.Conn.resp(conn, 200, schema_response)
+    end
+
+    {status, fields} = LastfmArchive.Load.check_solr_schema(:lastfm_ping_test2)
+
+    assert status == :ok
+    assert expected_fields == fields
+  end
+
+  test "should returns error if fields are missing in Solr schema check", %{bypass: bypass} do
+    url = "http://localhost:#{bypass.port}/"
+
+    Bypass.expect bypass, fn conn ->      
+      schema_response = File.read!(Path.join ["test","data", "schema_response_missing_fields.json"])
+      assert conn.path_info == ["schema"]
+      Plug.Conn.resp(conn, 200, schema_response)
+    end
+
+    assert {:error, %Hui.Error{reason: :einit} } = LastfmArchive.Load.check_solr_schema(url)
   end
 
 end
