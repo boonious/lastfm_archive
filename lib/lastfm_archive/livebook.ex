8f04be2430f4f13dca90126081ef1f29e5e4f5d5
Boon Low <boon.low@mac.com>
2023-06-06 17:11:43 +0100

    feat: archiving Livebook +progress status via playcounts heatmap, table

--- lib/lastfm_archive/livebook.ex
+++ lib/lastfm_archive/livebook.ex
@@ -3,12 +3,16 @@ defmodule LastfmArchive.Livebook do
   Livebook chart and text rendering.
   """
 
+  alias LastfmArchive.LastfmClient
+
+  @type monthy_count :: %{count: integer(), date: String.t()}
+
   @doc """
   Display user name and total number of scrobbles to archive.
   """
   @spec info :: Kino.Markdown.t()
   def info do
-    case {LastfmArchive.LastfmClient.default_user(), LastfmArchive.info()} do
+    case {LastfmClient.default_user(), LastfmArchive.info()} do
       {"", _} ->
         Kino.Markdown.new("""
         Please specify a Lastfm user in configuration.
@@ -24,5 +28,47 @@ defmodule LastfmArchive.Livebook do
         Unable to fetch user info from Lastfm API, have you configured the API key?
         """)
     end
+  end
+
+  @doc """
+  Returns a list of monthly counts of the scrobbles archived so far.
+  """
+  @spec status(module(), keyword()) :: list(monthy_count)
+  def status(cache \\ LastfmArchive.Cache, options \\ []) do
+    granularity = Keyword.get(options, :granularity, :monthly)
+
+    LastfmClient.default_user()
+    |> LastfmArchive.Cache.load(cache)
+    |> Enum.map(fn {{_user, year}, statuses} -> aggregate_counts(year, statuses, granularity) end)
+    |> List.flatten()
+  end
+
+  defp aggregate_counts(_year, statuses, :monthly) do
+    statuses
+    |> Enum.flat_map(fn {{from, _to}, {count, status}} ->
+      case Enum.all?(status, &(&1 == :ok)) do
+        true -> [{from, count}]
+        false -> []
+      end
+    end)
+    |> Enum.group_by(fn {from, _count} -> first_day_of_month(from) end, &elem(&1, 1))
+    |> Enum.into([], fn {day, counts} -> %{date: day, count: Enum.sum(counts)} end)
+  end
+
+  defp aggregate_counts(year, statuses, :yearly) do
+    statuses
+    |> Enum.flat_map(fn {{_from, _to}, {count, status}} ->
+      case Enum.all?(status, &(&1 == :ok)) do
+        true -> [count]
+        false -> []
+      end
+    end)
+    |> Enum.sum()
+    |> then(fn playcount -> %{year: year, count: playcount} end)
+  end
+
+  defp first_day_of_month(datetime) do
+    %DateTime{month: month, year: year} = datetime |> DateTime.from_unix!()
+    %Date{month: month, year: year, day: 1} |> to_string()
   end
 end
--- test/lastfm_archive/livebook_test.exs
+++ test/lastfm_archive/livebook_test.exs
@@ -1,8 +1,29 @@
 defmodule LastfmArchive.LivebookTest do
-  use ExUnit.Case, async: true
+  use ExUnit.Case
+
+  import Hammox
+
+  alias LastfmArchive.Cache
   alias LastfmArchive.Livebook, as: LFM_LB
+
+  @cache :livebooktest_cache
+
+  setup :set_mox_global
+  setup :verify_on_exit!
+
+  setup do
+    {:ok, _pid} = start_supervised(%{id: @cache, start: {Cache, :start_link, [[name: @cache]]}})
+    %{cache: %{{"a_user", 2006} => %{{1_138_752_000, 1_138_838_399} => {33, [:ok]}}}}
+  end
 
   test "info/0" do
     assert %Kino.Markdown{} = LFM_LB.info()
+  end
+
+  test "status/0", %{cache: cache} do
+    LastfmArchive.PathIOMock |> expect(:wildcard, fn _, _ -> [".cache_2006"] end)
+    LastfmArchive.FileIOMock |> expect(:read!, fn _ -> cache[{"a_user", 2006}] |> :erlang.term_to_binary() end)
+
+    assert [%{count: 33, date: "2006-02-01"}] == LFM_LB.status(@cache)
   end
 end
